2665e234a50138b0c386548fe05735561213f663
Ramkumar Pachaiyappan <ramkumar9007@gmail.com>
2018-02-19 23:02:53 +0530

    First commit

---
+++ app/src/main/java/com/view/list/facts/FactsActivity.java
@@ -0,0 +1,15 @@
+package com.view.list.facts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.view.list.R;
+
+public class FactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facts);
+    }
+}
